acadd8a8e78f8bd07d6f1071e2382f74cf3932e7
Custodio Serafim Langa <langacustodio@gmail.com>
2022-09-06 21:47:55 +0200

    Add style to display rockets list

--- src/components/rocket/Rocket.js
+++ src/components/rocket/Rocket.js
@@ -1,12 +1,13 @@
 import PropTypes from 'prop-types';
+import './Rocket.css';
 
 const Rocket = ({ rocket }) => (
-  <div>
+  <div className="container d-flex">
     <div>
-      <img src={rocket.flickr_images[0]} alt={rocket.name} />
+      <img className="rocketImages" src={rocket.flickr_images[0]} alt={rocket.name} />
     </div>
-    <div>
-      <h1>{rocket.name}</h1>
+    <div className="container">
+      <h3>{rocket.rocket_name}</h3>
       <p>
         {rocket.description}
       </p>
