32993ddc01294effb8f74bfdb98f014136eea705
PNdunguMaina <ndunguwamaina@gmail.com>
2022-09-06 19:35:20 +0300

    Implement basic structure for missions section

--- src/components/spaceHub/SpaceHub.js
+++ src/components/spaceHub/SpaceHub.js
@@ -1,10 +1,14 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navigation from '../Navigation/Navigation';
+import Missions from '../missions/Missions';
 
 const SpaceHub = () => (
   <Router>
     <div>
       <Navigation />
+      <Routes>
+        <Route path="/missions" element={<Missions />} />
+      </Routes>
     </div>
   </Router>
 );
