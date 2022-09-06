37eef41b0c81a87fa29ff0c9168ac499ab58bf9e
Custodio Serafim Langa <langacustodio@gmail.com>
2022-09-06 14:04:47 +0200

    Add rocket basic structure

--- src/components/spaceHub/SpaceHub.js
+++ src/components/spaceHub/SpaceHub.js
@@ -1,10 +1,20 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navigation from '../Navigation/Navigation';
+import Rockets from '../rocket/Rockets';
 
 const SpaceHub = () => (
   <Router>
     <div>
       <Navigation />
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={
+            <Rockets />
+                    }
+        />
+      </Routes>
     </div>
   </Router>
 );
