320f4820ba03b1ea2c8b6c4d8a8c732ceb468aea
Custodio Serafim Langa <langacustodio@gmail.com>
2022-09-08 14:24:44 +0200

    Add rocket tests

---
+++ src/test/Rockets.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Renderer from 'react-test-renderer';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../redux/configureStore';
+import Rockets from '../components/rocket/Rockets';
+
+
+test('renders rockets correctly', () => {
+  const tree = Renderer.create(
+    <React.StrictMode>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Rockets />
+        </BrowserRouter>
+      </Provider>
+    </React.StrictMode>,
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
