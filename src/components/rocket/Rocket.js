7afabe2aaae5478e45f91f659cfcb232b221d3a8
PNdunguMaina <ndunguwamaina@gmail.com>
2022-12-31 14:37:33 +0300

    Make rockets page responsive

--- src/components/rocket/Rocket.js
+++ src/components/rocket/Rocket.js
@@ -4,21 +4,25 @@ import RocketsBookingButton from './RocketsBookingButton';
 import './Rocket.css';
 
 const Rocket = ({ rocket }) => (
-  <div className="container d-flex">
-    <div>
-      <img className="rocketImages" src={rocket.flickr_images[0]} alt={rocket.name} />
+  <div className="container-fluid d-flex shadow-lg p-3 rounded my-3 rocket-div">
+    <div className="img-div">
+      <img
+        className="rocketImages w-100"
+        src={rocket.flickr_images[0]}
+        alt={rocket.name}
+      />
     </div>
     <div className="container">
       <h3>{rocket.rocket_name}</h3>
       <p>
-        {!rocket.reserved && (
-        <p>{rocket.description}</p>
-        )}
+        {!rocket.reserved && <p>{rocket.description}</p>}
         {rocket.reserved && (
           <p>
             {' '}
-            <Badge pill bg="info">Reserved</Badge>
-            {' '}
+            <Badge pill bg="info">
+              Reserved
+            </Badge>
+              {' '}
             {rocket.description}
           </p>
         )}
